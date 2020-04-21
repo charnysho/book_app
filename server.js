bae60c4e189b95b96d8abb247556a3c7c2471da1
charnysho <charnysho@gmail.com>
2020-04-21 13:15:52 -0700

    work with API

--- server.js
+++ server.js
@@ -3,13 +3,22 @@
 require('dotenv').config;
 
 const express = require('express');
+const superagent = require('superagent');
 const app = express();
-// const ejs = require('ejs');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
+
+function Book(data) {
+  this.search = data;
+  // this.title = data.title;
+  // this.author = data.author;
+  // this.description = data.description;
+}
+
+let books = [];
 
 app.set('view engine', 'ejs');
 
@@ -18,9 +27,35 @@ app.get('/', (request, response) => {
 });
 
 app.post('/search', (request, response) => {
-  response.render('./new.ejs', { root: './public' });
+  // response.render('./new.ejs', { root: './public' });
+  console.log(request.body.search);
+
+  let book = new Book(request.body.search);
+  // book.search = request.body.search;
+  books.push(book);
+  console.log(books);
+  response.render('./pages/index', { books: books, });
+
 });
 
+// function handleBooks(request, response) {
+//   response.render('index');
+  // console.log(request);
+  // const searchInput = request.query;
+  // const api_url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}&callback=handleResponse`;
+  // superagent.get(api_url)
+  //   .then(booksResponse => {
+  //     console.log('RESPONSE');
+  //     const data = booksResponse.body;
+  //     console.log(data);
+  //     response.send('Hi');
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+// }
+
+// app.get('/', handleBooks);
 app.listen(PORT, () => {
   console.log('App is up on port : ' + PORT);
 });
