d5cb0bf4784b18fd0452412d1e730c10874e0062
charnysho <charnysho@gmail.com>
2020-04-21 22:36:50 -0700

    lab11

--- server.js
+++ server.js
@@ -12,13 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 function Book(data) {
-  this.search = data;
-  // this.title = data.title;
-  // this.author = data.author;
-  // this.description = data.description;
+  this.title = data.title;
+  this.authors = data.authors;
+  this.description = data.description;
+  this.image_url = parseImageUrl(data.imageLinks.smallThumbnail);
 }
 
-let books = [];
+function parseImageUrl(imageUrl) {
+  if(imageUrl !== '') {
+    if(imageUrl.includes('http:')) {
+      return imageUrl.replace(/^http:/, 'https:');
+    } else {
+      return imageUrl;
+    }
+  }
+}
 
 app.set('view engine', 'ejs');
 
@@ -26,36 +34,37 @@ app.get('/', (request, response) => {
   response.render('./pages/index');
 });
 
-app.post('/search', (request, response) => {
-  // response.render('./new.ejs', { root: './public' });
-  console.log(request.body.search);
-
-  let book = new Book(request.body.search);
-  // book.search = request.body.search;
-  books.push(book);
-  console.log(books);
-  response.render('./pages/index', { books: books, });
-
+app.get('/searches/new', (request, response) => {
+  response.render('./pages/searches/new');
 });
 
-// function handleBooks(request, response) {
-//   response.render('index');
-  // console.log(request);
-  // const searchInput = request.query;
-  // const api_url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}&callback=handleResponse`;
-  // superagent.get(api_url)
-  //   .then(booksResponse => {
-  //     console.log('RESPONSE');
-  //     const data = booksResponse.body;
-  //     console.log(data);
-  //     response.send('Hi');
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //   });
-// }
+app.post('/searches', (request, response) => {
+  console.log(request.body);
+  const searchInput = request.body.searchInput;
+  const searchBy = request.body.searchBy;
+  console.log(searchInput);
+  console.log(searchBy);
+  let apiSearchTerm = (searchBy === 'title') ? 'intitle:' : 'inauthor:';
+  const api_url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}+${apiSearchTerm}`;
+  console.log(api_url);
+  superagent.get(api_url)
+    .then(booksResponse => {
+      console.log('RESPONSE');
+      const data = booksResponse.body.items;
+      const books = [];
+      data.map(item => books.push(new Book(item.volumeInfo)));
+      response.render('pages/searches/show', {books: books});
+    })
+    .catch(error => {
+      errorHandler(error, request, response);
+    });
+});
 
-// app.get('/', handleBooks);
+function errorHandler(error, request, response, next) {
+  console.log(error);
+  response.render('./pages/error', {error : 'Sorry something wrong'});
+}
+
 app.listen(PORT, () => {
   console.log('App is up on port : ' + PORT);
 });
