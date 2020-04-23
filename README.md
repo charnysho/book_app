e035e4c49a8fcc0f8f1b703995c9e12ddf6389f2
charnysho <charnysho@gmail.com>
2020-04-23 13:32:46 -0700

    lab 13

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # book_app
 
-
-Number and name of feature: ________________________________
-
-Estimate of time needed to complete: _____
-
-Start time: _____
-
-Finish time: _____
-expree
-Actual time needed to complete: _____
+##Features:
 
 
 **Author**: Volha Charnysh
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 'use strict';
 
 require('dotenv').config();
-const pg = require('pg');
-
-const express = require('express');
-const superagent = require('superagent');
-const methodOverride = require('method-override');
-const app = express();
-
 
 const PORT = process.env.PORT || 3000;
 
+const pg = require('pg');
+const express = require('express');
+const superagent = require('superagent');
+const methodOverride = require('method-override');
+
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 app.use(methodOverride('_method'));
@@ -19,11 +18,10 @@ const dbClient = new pg.Client(process.env.DATABASE_URL);
 
 function Book(data) {
   this.title = data.title;
-  this.authors = data.authors;
-  this.description = data.description;
+  this.author = data.author === undefined ? 'No Author Available' : data.author;
+  this.description = data.description === undefined ? 'No description Available' : data.description;
   this.image_url = parseImageUrl(data.imageLinks.smallThumbnail);
   this.isbn = `${data.industryIdentifiers[0].type} ${data.industryIdentifiers[0].identifier}`;
-  console.log(this);
 }
 
 function parseImageUrl(imageUrl) {
@@ -37,46 +35,40 @@ function parseImageUrl(imageUrl) {
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-  let selectQuery = 'SELECT * FROM books;';
-  return dbClient.query(selectQuery)
-    .then(results => {
-      if (results.rowCount === 0) {
-        console.log('RENDER FROM DB');
-        response.render('pages/searches/new');
-      } else {
-        response.render('pages/index', { books: results.rows, count: results.rowCount });
-      }
-    })
-    .catch(error => errorHandler(error, request, response));
+  getHomePage(request, response);
 });
+
+function getHomePage(request, response) {
+  getBooks(request, response, 'pages/index');
+}
 
 app.get('/searches/new', (request, response) => {
   response.render('./pages/searches/new');
 });
 
 app.get('/books', (request, response) => {
+  getBooks(request, response, 'pages/details');
+});
+
+function getBooks(request, response, pageUrl) {
   let selectQuery = 'SELECT * FROM books;';
   return dbClient.query(selectQuery)
     .then(results => {
       if (results.rowCount === 0) {
-        console.log('RENDER FROM DB');
         response.render('pages/searches/new');
       } else {
-        response.render('pages/details', { books: results.rows, count: results.rowCount });
+        response.render(pageUrl, { books: results.rows, count: results.rowCount });
       }
     })
     .catch(error => errorHandler(error, request, response));
-});
+}
 
-app.get('/books/:id', (request, response) => {
+app.get('/books/:id', getBooksId);
+
+function getBooksId(request, response) {
   const bookId = request.params.id;
-
-  request.query;
-  request.body;
-
   let selectQuery = `SELECT * FROM books WHERE id=$1;`;
   let selectValues = [bookId];
-
   dbClient.query(selectQuery, selectValues)
     .then(data => {
       if (data.rows.length > 0) {
@@ -86,7 +78,7 @@ app.get('/books/:id', (request, response) => {
       }
     })
     .catch(error => errorHandler(error, request, response));
-});
+}
 
 app.post('/searches', (request, response) => {
   const searchInput = request.body.searchInput;
@@ -108,10 +100,9 @@ app.post('/searches', (request, response) => {
 
 app.post('/books', (request, response) => {
   const { title, author, description, isbn, image_url, bookshelf } = request.body;
-
+  console.log(request.body);
   let addBookSQL = `INSERT INTO books (title, author, description, isbn, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
   let addBookValues = [title, author, description, isbn, image_url, bookshelf];
-
   dbClient.query(addBookSQL, addBookValues)
     .then(data => {
       response.redirect('books/' + data.rows[0].id);
@@ -122,14 +113,21 @@ app.post('/books', (request, response) => {
 app.put('/books/:id', (request, response) => {
   const bookId = request.params.id;
   const { title, author, description, image_url, isbn, bookshelf } = request.body;
-
-
   let SQL = `UPDATE books SET title=$1, author=$2, description=$3, image_url=$4, isbn=$5, bookshelf=$6 WHERE id=$7 RETURNING *`;
   let values = [title, author, description, image_url, isbn, bookshelf, bookId];
-
   dbClient.query(SQL, values)
     .then(data => {
-      response.send(data.rows);
+      response.render('pages/detail', { book: data.rows[0] });
+    })
+    .catch(error => errorHandler(error, request, response));
+});
+
+app.delete('/books/:id', (request, response) => {
+  const bookId = request.params.id;
+  let SQL = `DELETE FROM books WHERE id=$1 RETURNING *`;
+  dbClient.query(SQL, [bookId])
+    .then(data => {
+      response.redirect('/');
     })
     .catch(error => errorHandler(error, request, response));
 });
